ebd02a932d7fc0f3ed7c206cc236f95c716b3724
tommylay1902 <tommylay.c@gmail.com>
2022-09-02 10:01:43 -0700

    remove more useless code

--- src/main/java/com/example/security/SecurityConfiguration.java
+++ src/main/java/com/example/security/SecurityConfiguration.java
@@ -13,17 +13,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
+
+    //This bean will override the default security filter
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                //allow for testing api endpoints in postman
                 .csrf(c -> c.disable())
                 .authorizeRequests((auth) -> {
+                    //any path you try to access will need to make sure you are logged in first
                     auth.antMatchers("/*").authenticated();
+                    //from test controller, will permit any logged in user to access
                     auth.antMatchers("/hello").permitAll();
                     auth.antMatchers("/dashboard").hasRole("USER");
+                    //admin or user should have access to all orders
                     auth.antMatchers("/api/order/*").hasAnyRole("USER", "ADMIN");
+                    //admin only has access to racketadmins CRUD operations
                     auth.antMatchers("/api/racketadmin/*").hasRole("ADMIN");
                 })
+                //
                 .formLogin()
                 .and()
                 .httpBasic(withDefaults());
